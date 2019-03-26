586fb6e12dd2bda89ed89e52258adc6e7f77566a
MRo47 <rodriguesmyron47@gmail.com>
2019-03-26 15:17:13 +0000

    nothing much

--- test.py
+++ test.py
@@ -1,11 +1,18 @@
 import json
-from future.utils import iteritems
+from future.utils import viewitems
 
-dict = {'A':['B','C'],
-        'B':['D','E']}
+nodes = {'A':['B','C'],
+         'B':['D','E']}
 
-js = json.dumps(dict)
-print(dict['A'])
+js = json.dumps(nodes)
+print(nodes['A'])
 f = open("dict.json","w")
 f.write(js)
 f.close()
+
+for (key, value) in viewitems(nodes):
+    print(key, value)
+
+for key in nodes:
+    for value in nodes[key]:
+        print(value)
